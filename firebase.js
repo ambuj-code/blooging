d2e45c1dc7203a1ff4e1f6cf599b4b1a0b9aee07
ambuj-code <ambujsingh8780@gmail.com>
2024-10-13 15:11:34 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,15 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC9kB3-RQRKRgwMcwuPqtjDVlszTjyB2cA",
+    authDomain: "blogging-28b1c.firebaseapp.com",
+    projectId: "blogging-28b1c",
+    storageBucket: "blogging-28b1c.appspot.com",
+    messagingSenderId: "717068779127",
+    appId: "1:717068779127:web:2b86f35f68aa1e23a89072"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const database = firebase.database(app);
+
+
